5ca2c1a74092c79d6e9e541fe23ee65225fc4ad2
Rashika2308 <rashikavidyarthi545@gmail.com>
2025-06-14 08:20:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const container = document.getElementById('container');
-const colors = ['#3498db', '#e74c3c', '#2ecc71', '#f39c12', '#9b59b6'];
 
+// Create 800 squares
 for (let i = 0; i < 800; i++) {
     const square = document.createElement('div');
     square.className = 'square';
-    square.style.setProperty('--hover-color', colors[Math.floor(Math.random() * colors.length)]);
     container.appendChild(square);
 }
