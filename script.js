970c575d6fbe2d953fbbba9bfc5771a476d322bb
Rashika2308 <rashikavidyarthi545@gmail.com>
2025-06-14 08:16:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
 const container = document.getElementById('container');
-const COLORS = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
+const colors = ['#3498db', '#e74c3c', '#2ecc71', '#f39c12', '#9b59b6'];
 
 for (let i = 0; i < 800; i++) {
-  const square = document.createElement('div');
-  square.classList.add('square');
-
-  square.addEventListener('mouseenter', () => {
-    const color = getRandomColor();
-    square.style.setProperty('--hover-color', color);
-    square.classList.add('hovered');
-    setTimeout(() => {
-      square.classList.remove('hovered');
-    }, 1000);
-  });
-
-  container.appendChild(square);
+    const square = document.createElement('div');
+    square.className = 'square';
+    square.style.setProperty('--hover-color', colors[Math.floor(Math.random() * colors.length)]);
+    container.appendChild(square);
 }
-
-function getRandomColor() {
-  return COLORS[Math.floor(Math.random() * COLORS.length)];
-}
-
